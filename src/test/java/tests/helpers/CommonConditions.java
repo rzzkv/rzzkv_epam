a41067ba4fe744c0fded68492cb53d966bbadb63
rzzkv <rzzkv@yandex.ru>
2023-10-19 15:59:02 +0500

    del Tab class

--- src/test/java/tests/helpers/CommonConditions.java
+++ src/test/java/tests/helpers/CommonConditions.java
@@ -1,6 +1,5 @@
 package tests.helpers;
 
-import config.PropertiesFile;
 import org.assertj.core.api.SoftAssertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,16 +14,19 @@ import java.time.Duration;
 
 abstract public class CommonConditions {
 
-    //protected static WebDriver driver = null; //было бы логично сделать
-    //проблема с этой строкой, без нее не работают тест сюиты, но с ней открывается два браузера
-    protected static WebDriver driver = new ChromeDriver();
     protected SoftAssertions softly = new SoftAssertions();
     protected Utility utility=new Utility(driver);
-    public static String browserName = null;
+
+    //protected static WebDriver driver = null; //было бы логично сделать
+    protected static WebDriver driver = new ChromeDriver();
+    //проблема с этой строкой, без нее не работают тест сюиты, но с ней открывается два браузера
+
+    //chose browser
+    public static String browserName = "firefox";
 
     @BeforeMethod
     public void setUp() {
-        PropertiesFile.getProperties();
+//        PropertiesFile.getProperties();
         if (browserName.equalsIgnoreCase("chrome")) {
             System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
             driver = new ChromeDriver();
